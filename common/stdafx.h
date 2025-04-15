30e8391f70584c925fa80e674fbf15e41f635f9b
Dirk Zimmermann <me@dirkz.com>
2025-04-15 10:02:21 +0200

    Add common (precompiled headers), make win32 app

---
+++ common/stdafx.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <comdef.h>
+#include <wrl.h>
+
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <d3dcompiler.h>
+#include <dxgi1_6.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include <array>
+#include <chrono>
+#include <cstdlib>
+#include <exception>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <map>
+#include <memory>
+#include <numbers>
+#include <span>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "Error.h"
+
+using Microsoft::WRL::ComPtr;
---
+++ main.cpp
@@ -0,0 +1,7 @@
+﻿#include "stdafx.h"
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                           _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    OutputDebugString(L"Hello, world!\n");
+}
