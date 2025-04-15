20d5c43c5ba2a55851c785db7c757225ce9b9324
Dirk Zimmermann <me@dirkz.com>
2025-04-15 11:34:31 +0200

    Wrap this into a class

--- Window.h
+++ Window.h
@@ -8,63 +8,66 @@
 namespace dxultra11
 {
 
-template <class T> struct WindowState
+template <class T> struct Window
 {
-    T *pCallBack = nullptr;
-};
-
-template <class T> LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    WindowState<T> *pState = nullptr;
-
-    if (uMsg == WM_CREATE)
+    struct WindowState
     {
+        T *pCallBack = nullptr;
+    };
+
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        WindowState *pState = nullptr;
+
+        if (uMsg == WM_CREATE)
+        {
+        }
+
+        return TRUE;
     }
 
-    return TRUE;
-}
-
-template <class T> void OpenWindow(HINSTANCE hInstance)
-{
-    std::wstring classname = T::WindowClass();
-    std::wstring title = T::WindowTitle();
-
-    WNDCLASS wc{};
-    wc.hInstance = hInstance;
-    wc.lpszClassName = classname.c_str();
-    wc.lpfnWndProc = WindowProc<T>;
-
-    RECT rect;
-    LPRECT pRect = T::DesiredWindowRect(&rect);
-
-    int x = CW_USEDEFAULT;
-    int y = CW_USEDEFAULT;
-    int width = CW_USEDEFAULT;
-    int height = CW_USEDEFAULT;
-
-    if (pRect)
+    static void Create(HINSTANCE hInstance)
     {
-        x = pRect->left;
-        y = pRect->top;
-        width = pRect->right - pRect->left;
-        height = pRect->bottom - pRect->top;
+        std::wstring classname = T::WindowClass();
+        std::wstring title = T::WindowTitle();
+
+        WNDCLASS wc{};
+        wc.hInstance = hInstance;
+        wc.lpszClassName = classname.c_str();
+        wc.lpfnWndProc = WindowProc;
+
+        RECT rect;
+        LPRECT pRect = T::DesiredWindowRect(&rect);
+
+        int x = CW_USEDEFAULT;
+        int y = CW_USEDEFAULT;
+        int width = CW_USEDEFAULT;
+        int height = CW_USEDEFAULT;
+
+        if (pRect)
+        {
+            x = pRect->left;
+            y = pRect->top;
+            width = pRect->right - pRect->left;
+            height = pRect->bottom - pRect->top;
+        }
+
+        WindowState *pWindowState = new WindowState();
+
+        HWND hwnd = CreateWindowEx(0,                   // Optional window styles.
+                                   classname.c_str(),   // Window class
+                                   title.c_str(),       // Window text
+                                   WS_OVERLAPPEDWINDOW, // Window style
+
+                                   // Size and position
+                                   x, y, width, height,
+
+                                   NULL,        // Parent window
+                                   NULL,        // Menu
+                                   hInstance,   // Instance handle
+                                   pWindowState // Additional application data
+        );
     }
-
-    WindowState<T> *pWindowState = new WindowState<T>();
-
-    HWND hwnd = CreateWindowEx(0,                   // Optional window styles.
-                               classname.c_str(),   // Window class
-                               title.c_str(),       // Window text
-                               WS_OVERLAPPEDWINDOW, // Window style
-
-                               // Size and position
-                               x, y, width, height,
-
-                               NULL,        // Parent window
-                               NULL,        // Menu
-                               hInstance,   // Instance handle
-                               pWindowState // Additional application data
-    );
 };
 
 } // namespace dxultra11
--- main.cpp
+++ main.cpp
@@ -9,5 +9,5 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
                     _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
 {
     OutputDebugString(L"Hello, world!\n");
-    OpenWindow<DXUltra>(hInstance);
+    Window<DXUltra>::Create(hInstance);
 }
